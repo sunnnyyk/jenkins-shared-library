7782ff9edcef37aee509ca4650f97285fca6250c
sunnnyyk <153540154+sunnnyyk@users.noreply.github.com>
2025-05-11 18:49:43 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,33 +1,4 @@
-def call(Map config = [:]) {
-def repoUrl = config.repoUrl
-def branch = config.branch ?: 'master'
-def mavenCommand = config.mavenCommand ?: 'clean package'
-
-    if (!repoUrl) {
-        error "Repository URL is required!"
-    }
-
-    pipeline {
-        agent any
-
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    git branch: branch, url: repoUrl
-                }
-            }
-
-            stage('Run Maven Command') {
-                steps {
-                    sh "mvn ${mavenCommand}"
-                }
-            }
-        }
-
-        post {
-            always {
-                echo 'Build finished.'
-            }
-        }
-    }
+def call(Map config) {
+    git branch: config.branch, url: config.repoUrl
+    sh "mvn ${config.mavenCommand}"
 }
